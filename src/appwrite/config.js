ab29e70528880f2b7d7780d99cf243c711472560
sirishdasari <dasarisirish@gmail.com>
2023-03-22 04:51:52 -0500

    env

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -3,7 +3,7 @@ import { Client, Graphql } from "appwrite";
 // initialize SDK
 const client = new Client();
 client
-    .setEndpoint('http://64.227.178.153/v1')
-    .setProject('641aa146f117942c8c52');
+    .setEndpoint(process.env.REACT_APP_URL)
+    .setProject(process.env.REACT_APP_ID);
 
 export const graphql = new Graphql(client);
